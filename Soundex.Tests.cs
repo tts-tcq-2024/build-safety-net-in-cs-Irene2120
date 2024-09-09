8e2bcfc796a99b3d5a1f8c7b69477a3ff8d5ec99
Irene2120 <85551810+Irene2120@users.noreply.github.com>
2024-09-09 20:49:15 +0530

    Update Soundex.Tests.cs

--- Soundex.Tests.cs
+++ Soundex.Tests.cs
@@ -1,8 +1,32 @@
 using Xunit;
+using System;
+using System.Text;
 
 public class SoundexTests
 {
     [Fact]
+    public void GenerateSoundex_EmptyString_ReturnsEmptyString()
+    {
+        string input=""; //arrange
+
+        string result = Soundex.GenerateSoundex(input);   //Act
+
+        Assert.Equal(string.Empty,result);      //assert
+    }
+    public void GenerateSoundex_SingleCharacter_ReturnsPaddedCode()
+    {
+        string input ="A"; //arrange
+        string result =Soundex.GenerateSoundex(input);   //act
+        Assert.Equal("A000",result);         //act
+    }
+
+    public void GenerateSoundex_ValidName_ReturnsCorrectSoundex()
+    {
+        string input="Abcd";
+        string result = Soundex.GenerateSoundex(input);
+        Assert.Equal("0123",result);
+    }
+            
     public void HandlesEmptyString()
     {
         Assert.Equal(string.Empty, Soundex.GenerateSoundex(""));
